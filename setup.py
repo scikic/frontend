931a9afe1cdc17a466bfb2ecc2ce70328fbc6394
Mike Smith <mike@michaeltsmith.org.uk>
2015-09-01 19:13:19 +0000

    Added change to sessions table, to include facebook id

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if not os.path.exists(config.pathToData):
 print "Configuring psych database"
 con = lite.connect(config.pathToData + 'psych.db')
 cur = con.cursor()
-cur.execute('CREATE TABLE IF NOT EXISTS sessions (user integer primary key autoincrement, sessionid varchar(255));')
+cur.execute('CREATE TABLE IF NOT EXISTS sessions (user integer primary key autoincrement, sessionid varchar(255), facebookid varchar(255));')
 cur.execute('CREATE TABLE IF NOT EXISTS conversation_state (sessionid varchar(255) primary key, state integer);')
 #cur.execute('CREATE TABLE IF NOT EXISTS qa (userid integer, dataset varchar(255), dataitem varchar(255), detail varchar(255), answered integer, asked_last integer, answer varchar(255), PRIMARY KEY (userid, dataset, dataitem, detail));')
 cur.execute('CREATE TABLE IF NOT EXISTS qa (qid integer PRIMARY KEY AUTOINCREMENT, userid integer, dataset varchar(255), dataitem varchar(255), detail varchar(255), answered integer, asked_last integer, answer varchar(255), processed integer);')
