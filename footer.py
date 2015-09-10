7782b5ce33803fd72e9fb9ec86516c89834f467c
Mike Smith <mike@michaeltsmith.org.uk>
2015-09-10 08:59:07 +0000

    Adding facebook link

--- footer.py
+++ footer.py
@@ -1,6 +1,5 @@
 def text():
-    res = ''
-    res+= '<span class="footertextright">Background image provided by <a href="https://www.flickr.com/photos/kwarz/14628848258">zeitfaenger.at</a>. Avatar image provided by <a href="http://clipartist.net/svg/emily-the-strange-emily-the-strange-awesome-rss-openclipart-org-commons-wikimedia-org/">clipartist.net</a>.</span>';
-    res+= '<span class="footertextleft"><a href="index.cgi">Home</a> | <a href="privacy.html">Privacy</a> | <a href="tos.html">Terms</a> | <a href="about.html">About</a> | <a href="account.cgi">Account</a></span>';
-    res+= '<span class="headerlogoright"> <img src="logo.png" /></span>';
+    res = ''    
+    res+= '<span class="footertextleft"><a href="index.cgi">Home</a> | <a href="privacy.html">Privacy</a> | <a href="tos.html">Terms</a> | <a href="about.html">About</a> | <a href="account.cgi">Account</a> | <a href="#"><span class="fbshare">share on facebook</span></a></span>';
+    res+= '<span class="headerlogoright"> <img class="logo" src="logo.png" /></span>';
     return res
